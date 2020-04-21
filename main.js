96b7c0949b8e0fccfac77d83b977c53e6b8c831b
Wojciech Antonik <antonik.wojciech2000@gmail.com>
2020-04-21 19:42:44 +0200

    create displayTeam

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-const addTeamForm = document.querySelector('.addTeams__form')
-
 class Teams {
 
-    static ShowTeam(teamName) 
+    static AddTeam(teamName) 
     {
 
         const spaceForTeams = document.querySelector('.addTeams__spaceForTeams')
@@ -12,6 +10,11 @@ class Teams {
         teamDiv.classList.add('addTeams__addedTeam')
 
         spaceForTeams.appendChild(teamDiv)
+
+        const tl = gsap.timeline()
+
+        tl.from(teamDiv, { opacity: 0, x: 30, duration: .5 })
+
     }
 
     static ShowMessage(message)
@@ -43,7 +46,7 @@ class Teams {
 
     }
 
-    static checkName(teamName)
+    static CheckName(teamName)
     {
         const teamNames = document.querySelectorAll('.addTeams__addedTeam')
 
@@ -56,27 +59,166 @@ class Teams {
 
     }
 
+    static RemoveTeam(teamName)
+    {
+        const tl = gsap.timeline()
+
+        tl.to(teamName, { opacity: 0, x: 30, duration: .5 })
+
+        setTimeout(()=> teamName.remove(), 500)
+
+    }
+
 }
 
-addTeamForm.addEventListener('submit', e => {
+class DisplayTeams {
+
+    static ShuffleTeams(teams)
+    {
+        let shuffledTeams = []
+
+        for(let i of teams)
+        {
+            shuffledTeams.push(i.textContent)
+        }
+
+        let newPos, temp
+
+        for(let i = shuffledTeams.length - 1; i > 0; i--)
+        {
+            newPos = Math.floor(Math.random() * (i+1))
+            temp = shuffledTeams[i]
+            shuffledTeams[i] = shuffledTeams[newPos]
+            shuffledTeams[newPos] = temp
+        }
+
+        DisplayTeams.CreateTable(shuffledTeams)
+
+    }
+
+    static CreateTable(teams)
+    {
+        const container = document.querySelector('.container')
+        const addTeams = document.querySelector('.addTeams')
+
+        const eliminationTable = document.createElement('div')
+
+        eliminationTable.classList.add('displayTeams')
+
+        const tl = gsap.timeline()
+        
+        tl.to(addTeams, {opacity: 0, duration: .5})
+        
+        setTimeout(()=> {
+
+            tl.from(eliminationTable, {opacity: 0, duration: .5})
+
+            container.replaceChild(eliminationTable,addTeams)
+
+            let teamsName = []
+    
+            for(let i = teams.length; i >= 1; i /= 2)
+            {
+                teamsName.push(i)
+            }
+    
+            for(let i = teamsName.length; i > 0; i--)
+            {
+                const div = document.createElement('div')
+                div.classList.add('displayTeams__ladder')
+    
+                eliminationTable.appendChild(div)
+            }
+    
+            teamsName.forEach((teamRowNumber,index) => {
+    
+            const teamsLadder = document.querySelectorAll('.displayTeams__ladder')
+    
+            for(let i = teamRowNumber; i > 0; i--)
+            {
+                const div = document.createElement('div')
+    
+                div.classList.add('displayTeams__teamName')
+    
+                teamsLadder[index].appendChild(div)
+            }
+    
+            if(index === 0)
+            {
+                Array.from(teamsLadder[0].children).forEach((team,index) => {
+                team.classList.add('displayTeams__teamName')
+                team.textContent = teams[index]
+                })
+            }
+    
+            })
+
+        },500)
+
+
+    }
+
+}
+
+
+const addTeamButton = document.querySelector('.addTeams__button')
+
+addTeamButton.addEventListener('click', e => {
 
     e.preventDefault()
 
     const teamName = document.querySelector('.addTeams__teamName')
 
-    const checkDuplication = Teams.checkName(teamName.value)
+    const checkDuplication = Teams.CheckName(teamName.value)
 
     if(!teamName.value)
     {
-        Teams.ShowMessage('UZUPEŁNIJ POLE')
+        Teams.ShowMessage('Uzupełnij pole')
     }
     else if(checkDuplication)
     {
-        Teams.ShowMessage('TAKA NAZWA DRUŻYNY JUŻ ISTNIEJE')
+        Teams.ShowMessage('Taka nazwa drużyny już istnieje')
+    }
+    else if(teamName.value.length > 11)
+    {
+        Teams.ShowMessage('za długa nazwa drużyny (max 11 znaków)')
     }
     else if(teamName.value)
     {
-        Teams.ShowTeam(teamName.value)
+        Teams.AddTeam(teamName.value)
         teamName.value = ''
     }
+
+    const teams = document.querySelectorAll('.addTeams__addedTeam')
+
+    teams.forEach(team => {
+
+        team.addEventListener('click', e => {
+            
+            Teams.RemoveTeam(e.target)
+
+        })
+
+    })
+
+})
+
+
+const teamForm = document.querySelector('.addTeams__form')
+
+teamForm.addEventListener('submit', e => {
+
+    e.preventDefault()
+
+    const teams = document.querySelectorAll('.addTeams__addedTeam')
+
+    if(teams.length === 2 || teams.length === 4 || teams.length === 8 || teams.length === 16)
+    {
+        DisplayTeams.ShuffleTeams(teams)
+    }
+    else
+    {
+        Teams.ShowMessage("Dostępna liczba drużyn to: 2,4,8,16")
+    }
+
 })
